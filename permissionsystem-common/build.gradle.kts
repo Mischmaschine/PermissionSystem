b6246a1ac6e013b557a4186e03f9b340e91adc00
Mischmschine <77553282+Mischmschine@users.noreply.github.com>
2022-07-10 21:12:01 +0200

    update build.gradle.kts

--- permissionsystem-common/build.gradle.kts
+++ permissionsystem-common/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.10"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 group = "de.permissionsystem"
@@ -19,9 +20,9 @@ tasks {
 
 dependencies {
     implementation("com.github.Mischmaschine:DatabaseLib:dev1.0-SNAPSHOT")
-    implementation("com.google.code.gson:gson:2.9.0")
     implementation("org.mongodb:mongodb-driver-sync:4.6.0")
     compileOnly("org.mariadb.jdbc:mariadb-java-client:3.0.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 }
 
 tasks.getByName<Test>("test") {
