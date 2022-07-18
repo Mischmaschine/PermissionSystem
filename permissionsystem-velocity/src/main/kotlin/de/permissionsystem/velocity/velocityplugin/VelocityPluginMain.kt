0f595a3a9d41a74ebccf22a19e089b105163dccc
Mischmaschine <oxsSKRIsGELU>
2022-07-18 23:30:21 +0200

    feat: added message channel to synchronize the spigot with the proxy

--- permissionsystem-velocity/src/main/kotlin/de/permissionsystem/velocity/velocityplugin/VelocityPluginMain.kt
+++ permissionsystem-velocity/src/main/kotlin/de/permissionsystem/velocity/velocityplugin/VelocityPluginMain.kt
@@ -1,5 +1,6 @@
-package de.permissionsystem.velocity
+package de.permissionsystem.velocity.velocityplugin
 
+import com.google.common.io.ByteStreams
 import com.google.inject.Inject
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.permission.PermissionsSetupEvent
@@ -7,43 +8,36 @@ import com.velocitypowered.api.event.player.ServerConnectedEvent
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.plugin.Plugin
 import com.velocitypowered.api.plugin.annotation.DataDirectory
+import com.velocitypowered.api.proxy.Player
 import com.velocitypowered.api.proxy.ProxyServer
+import de.permissionsystem.velocity.VelocityCommandManagerSurrogate
+import de.permissionsystem.velocity.command.PermissionCommand
 import de.permissionsystem.velocity.permission.PermissionProviderSurrogate
-import net.kyori.adventure.text.Component
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import permission.PermissionInitializer
+import permission.player.PermissionPlayer
 import java.nio.file.Path
-import java.util.concurrent.TimeUnit
 import kotlin.io.path.absolutePathString
 
 @Plugin(id = "permissionsystem", name = "PermissionSystem", version = "1.0")
 class VelocityPluginMain @Inject constructor(
-    private val proxyServer: ProxyServer,
+    val proxyServer: ProxyServer,
     @DataDirectory private val dataDirectory: Path
 ) {
 
+    val json = Json.Default
     lateinit var permissionProviderSurrogate: PermissionProviderSurrogate
     lateinit var permissionInitializer: PermissionInitializer
 
     @Subscribe
     fun onProxyInitialize(event: ProxyInitializeEvent) {
+        instance = this
         this.permissionInitializer = PermissionInitializer(dataDirectory.absolutePathString())
         this.permissionProviderSurrogate = PermissionProviderSurrogate(permissionInitializer.permissionPlayerManager)
-        proxyServer.scheduler.buildTask(this) {
-            println("PermissionSystem: PermissionSystem is now ready!")
-            proxyServer.allPlayers.forEach { player -> player.sendMessage(Component.text("PermissionSystem: Permissions loaded")) }
-            println(permissionInitializer.permissionPlayerManager.getAllCachedPermissionPlayers())
-            println(permissionInitializer.permissionPlayerManager.getAllCachedPermissionPlayers())
-            permissionInitializer.permissionPlayerManager.getAllCachedPermissionPlayers().forEach {
-                val player = proxyServer.getPlayer(it.uuid)
-
-                player.get().sendMessage(
-                    Component.text(
-                        "${it.getPermissions()}"
-                    )
-                )
-
-            }
-        }.repeat(2, TimeUnit.SECONDS).schedule()
+        val commandManager = VelocityCommandManagerSurrogate(proxyServer, this)
+        commandManager.enableUnstableAPI("help")
+        commandManager.registerCommand(PermissionCommand(this, permissionInitializer.permissionGroupManager))
     }
 
     @Subscribe
@@ -54,5 +48,24 @@ class VelocityPluginMain @Inject constructor(
     @Subscribe
     fun onServerConnected(event: ServerConnectedEvent) {
         permissionInitializer.permissionPlayerManager.getPermissionPlayer(event.player.uniqueId)
+    }
+
+    fun publishData(player: Player, permissionPlayer: PermissionPlayer) {
+        proxyServer.allPlayers?.let {
+            if (it.isEmpty()) {
+                return
+            }
+            player.sendPluginMessage(
+                { "player:permsUpdate" },
+                ByteStreams.newDataOutput()
+                    .also { dataOutputStream -> dataOutputStream.writeUTF(json.encodeToString(permissionPlayer)) }
+                    .toByteArray()
+            )
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        lateinit var instance: VelocityPluginMain
     }
 }
