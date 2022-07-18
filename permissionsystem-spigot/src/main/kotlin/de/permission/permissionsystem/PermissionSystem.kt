1a5e7b2737a1b8a34afabf99b28b9434861813d5
Mischmschine <77553282+Mischmschine@users.noreply.github.com>
2022-07-18 19:43:24 +0200

    chore: replaced gson with kotlinx

--- permissionsystem-spigot/src/main/kotlin/de/permission/permissionsystem/PermissionSystem.kt
+++ permissionsystem-spigot/src/main/kotlin/de/permission/permissionsystem/PermissionSystem.kt
@@ -1,9 +1,9 @@
 package de.permission.permissionsystem
 
 import com.google.common.io.ByteStreams
-import com.google.gson.Gson
 import de.permission.listener.event.EventRegistrationService
-import org.bukkit.Bukkit
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.plugin.messaging.PluginMessageListener
@@ -20,20 +20,6 @@ class PermissionSystem : JavaPlugin(), PluginMessageListener {
         this.permissionPlayerManager = permissionInitializer.permissionPlayerManager
         EventRegistrationService(this, permissionInitializer.permissionPlayerManager)
         server.messenger.registerIncomingPluginChannel(this, "player:permsUpdate", this)
-        Bukkit.getScheduler().runTaskTimerAsynchronously(this, Runnable {
-            permissionPlayerManager.getAllCachedPermissionPlayers().forEach {
-                Bukkit.broadcastMessage(
-                    "Permission: ${
-                        it.getAllNotExpiredPermissions().map { permission -> permission.name }
-                    }"
-                )
-                Bukkit.broadcastMessage(
-                    "Permission: ${
-                        it.getPermissions().map { permission -> permission.getRemainingTime() }
-                    }"
-                )
-            }
-        }, 0, 45)
     }
 
     override fun onPluginMessageReceived(channel: String, player: Player, bytes: ByteArray) {
@@ -43,7 +29,7 @@ class PermissionSystem : JavaPlugin(), PluginMessageListener {
         ByteStreams.newDataInput(bytes).let {
             val permissionPlayerJson = it.readUTF()
             println(permissionPlayerJson)
-            val permissionPlayer = Gson().fromJson(permissionPlayerJson, PermissionPlayer::class.java)
+            val permissionPlayer = Json.decodeFromString<PermissionPlayer>(permissionPlayerJson)
             permissionPlayer.updateCache()
         }
     }
