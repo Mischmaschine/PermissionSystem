d85258558a7ca401259ae608c31916c209357a09
Mischmschine <77553282+Mischmschine@users.noreply.github.com>
2022-07-18 19:42:41 +0200

    chore: the permission player will be automatically  serialized

--- permissionsystem-bungeecord/src/main/kotlin/de/permission/permissionsystem/PermissionSystem.kt
+++ permissionsystem-bungeecord/src/main/kotlin/de/permission/permissionsystem/PermissionSystem.kt
@@ -3,12 +3,17 @@ package de.permission.permissionsystem
 import com.google.common.io.ByteStreams
 import de.permission.BungeeCommandManagerSurrogate
 import de.permission.listener.event.EventRegistrationService
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import net.md_5.bungee.api.ProxyServer
 import net.md_5.bungee.api.connection.ProxiedPlayer
 import net.md_5.bungee.api.plugin.Plugin
 import permission.PermissionInitializer
+import permission.player.PermissionPlayer
 
 class PermissionSystem : Plugin() {
+
+    val json = Json.Default
 
     override fun onEnable() {
         val permissionInitializer = PermissionInitializer(this.dataFolder.absolutePath)
@@ -23,18 +28,17 @@ class PermissionSystem : Plugin() {
         )
         this.logger.info("PermissionSystem has been enabled")
         proxy.registerChannel("player:permsUpdate")
-
-
     }
 
-    fun publishData(player: ProxiedPlayer, permissionJson: String) {
+    fun publishData(player: ProxiedPlayer, permissionPlayer: PermissionPlayer) {
         val networkPlayers = ProxyServer.getInstance().players
         networkPlayers?.let {
             if (it.isEmpty()) {
                 return
             }
             player.server.info.sendData("player:permsUpdate",
-                ByteStreams.newDataOutput().also { dataOutputStream -> dataOutputStream.writeUTF(permissionJson) }
+                ByteStreams.newDataOutput()
+                    .also { dataOutputStream -> dataOutputStream.writeUTF(json.encodeToString(permissionPlayer)) }
                     .toByteArray()
             )
         }
