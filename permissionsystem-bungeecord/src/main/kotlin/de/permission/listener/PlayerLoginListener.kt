60b76c4d966b6b0a880c5a543332e2bff55343cc
Mischmschine <77553282+Mischmschine@users.noreply.github.com>
2022-07-18 19:41:03 +0200

    fix: event is now completed if the asynchronous task is finished

--- permissionsystem-bungeecord/src/main/kotlin/de/permission/listener/PlayerLoginListener.kt
+++ permissionsystem-bungeecord/src/main/kotlin/de/permission/listener/PlayerLoginListener.kt
@@ -5,6 +5,7 @@ import de.permission.permissionsystem.PermissionSystem
 import net.md_5.bungee.api.event.LoginEvent
 import net.md_5.bungee.api.plugin.Listener
 import net.md_5.bungee.event.EventHandler
+import net.md_5.bungee.event.EventPriority
 import permission.extensions.update
 import permission.player.PermissionPlayer
 import permission.player.manager.PermissionPlayerManager
@@ -14,7 +15,7 @@ internal class PlayerLoginListener(
     private val permissionPlayerManager: PermissionPlayerManager
 ) : Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     fun onPlayerLogin(event: LoginEvent) {
 
         event.registerIntent(permissionSystem)
@@ -23,13 +24,13 @@ internal class PlayerLoginListener(
             it.getPermissions().filter { permission -> permission.isExpired() }.forEach(it::removePermission)
                 .also { _ ->
                     it.update()
+                    event.completeIntent(permissionSystem)
                 }
         }.onFailure {
             permissionPlayerManager.createPermissionPlayer(
                 PermissionPlayer(event.connection.uniqueId)
             )
+            event.completeIntent(permissionSystem)
         }
-        event.completeIntent(permissionSystem)
-
     }
 }
