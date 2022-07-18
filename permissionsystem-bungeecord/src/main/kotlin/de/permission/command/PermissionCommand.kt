190576092e9fff5fd03dc344672bb49e3a218408
Mischmschine <77553282+Mischmschine@users.noreply.github.com>
2022-07-18 19:49:56 +0200

    feat: added descriptions to all commands

--- permissionsystem-bungeecord/src/main/kotlin/de/permission/command/PermissionCommand.kt
+++ permissionsystem-bungeecord/src/main/kotlin/de/permission/command/PermissionCommand.kt
@@ -21,14 +21,16 @@ class PermissionCommand(
 ) : BaseCommand() {
 
     @CommandAlias("help")
-    @Default
+    //@Default
     @HelpCommand
+    @Description("Shows help for the permission command")
     fun onHelp(commandHelp: CommandHelp) {
         commandHelp.showHelp()
     }
 
     @Subcommand("group create")
     @Syntax("<name>")
+    @Description("Create a new group")
     fun onGroupCreate(commandSender: CommandSender, groupName: String) {
         permissionGroupManager.getPermissionGroup(groupName).onSuccess {
             commandSender.sendMessage(TextComponent("Group already exists"))
@@ -40,6 +42,7 @@ class PermissionCommand(
 
     @Subcommand("group delete")
     @Syntax("<name>")
+    @Description("Deletes a group")
     fun onGroupDelete(commandSender: CommandSender, groupName: String) {
         permissionGroupManager.getPermissionGroup(groupName).onSuccess {
             permissionGroupManager.deletePermissionGroup(groupName)
@@ -52,6 +55,7 @@ class PermissionCommand(
     @Subcommand("user")
     @CommandCompletion("@players permission|group add|remove")
     @Syntax("<player> <permission|group> <add|remove> <groupName|permissionName> <timeout>")
+    @Description("Add or remove a permission or group to a player")
     fun onUser(
         commandSender: CommandSender,
         playerName: String,
@@ -68,19 +72,32 @@ class PermissionCommand(
             commandSender.sendMessage(TextComponent("§cPlayer not found"))
             return
         }
+        println(playerName)
+        println(addition)
+        println(action)
+        println(typeName)
+        println(timeout)
         addition?.let { addition ->
             when (addition.lowercase()) {
                 "permission" -> {
                     action?.let { action ->
                         when (action.lowercase()) {
                             "add" -> {
+                                println("add123")
                                 timeout?.let { long ->
+                                    println("timeout1234")
                                     typeName?.let { typeName ->
+                                        println("typeNameaqe23423")
                                         player.getPermissionPlayer().onSuccess {
+                                            println("success")
                                             val permission = Permission(typeName, long)
+                                            println("success132")
                                             it.addPermission(permission)
-                                            permissionSystem.publishData(player, it.encodeToString())
+                                            println("succes345345üüüüüüüüüüüs")
+                                            permissionSystem.publishData(player, it)
+                                            println("successpubluishads")
                                             it.update()
+                                            println("updated")
                                             commandSender.sendMessage(TextComponent("§aPermission added"))
 
                                         }.onFailure {
@@ -95,7 +112,7 @@ class PermissionCommand(
                                 typeName?.let { typeName ->
                                     player.getPermissionPlayer().onSuccess {
                                         it.removePermission(typeName)
-                                        permissionSystem.publishData(player, it.encodeToString())
+                                        permissionSystem.publishData(player, it)
                                         it.update()
                                         commandSender.sendMessage(TextComponent("§aPermission removed"))
                                     }.onFailure {
@@ -120,7 +137,7 @@ class PermissionCommand(
                                         player.getPermissionPlayer().onSuccess {
                                             val infoGroup = PermissionInfoGroup(typeName, long)
                                             it.addPermissionInfoGroup(infoGroup)
-                                            permissionSystem.publishData(player, it.encodeToString())
+                                            permissionSystem.publishData(player, it)
                                             it.update()
                                             commandSender.sendMessage(TextComponent("§aGroup added"))
 
@@ -135,7 +152,7 @@ class PermissionCommand(
                                 typeName?.let { typeName ->
                                     player.getPermissionPlayer().onSuccess {
                                         it.removePermissionInfoGroup(typeName)
-                                        permissionSystem.publishData(player, it.encodeToString())
+                                        permissionSystem.publishData(player, it)
                                         it.update()
                                         commandSender.sendMessage(TextComponent("§aGroup removed"))
                                     }.onFailure {
