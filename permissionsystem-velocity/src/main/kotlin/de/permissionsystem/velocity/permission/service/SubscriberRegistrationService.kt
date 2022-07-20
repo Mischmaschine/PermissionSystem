e828ca2f15174a378245ef31e30b16a16ca44e8a
Mischmschine <77553282+Mischmschine@users.noreply.github.com>
2022-07-20 13:44:50 +0200

    chore: added deprecation suppress annotation

--- permissionsystem-velocity/src/main/kotlin/de/permissionsystem/velocity/permission/service/SubscriberRegistrationService.kt
+++ permissionsystem-velocity/src/main/kotlin/de/permissionsystem/velocity/permission/service/SubscriberRegistrationService.kt
@@ -18,6 +18,7 @@ class SubscriberRegistrationService(
 ) {
 
     init {
+        @Suppress("DEPRECATION")
         velocityCommandManager.enableUnstableAPI("help")
         registerSubscriber(PlayerServerConnectedSubscriber(permissionPlayerManager))
         registerCommand(PermissionCommand(velocityPluginMain, permissionGroupManager))
