eff2a9fb9f134a402f1a4b9e846d9edf229d4dfb
Mischmaschine <fixxertrixxer@gmail.com>
2022-07-25 16:00:32 +0200

    style: edit command response

--- permissionsystem-velocity/src/main/kotlin/de/permissionsystem/velocity/VelocityCommandManagerSurrogate.kt
+++ permissionsystem-velocity/src/main/kotlin/de/permissionsystem/velocity/VelocityCommandManagerSurrogate.kt
@@ -14,8 +14,8 @@ class VelocityCommandManagerSurrogate(proxyServer: ProxyServer, plugin: Any) :
             put(MessageKeys.PERMISSION_DENIED, "You don't have permission to do that.")
             put(MessageKeys.PERMISSION_DENIED_PARAMETER, "You don't have permission to do that with {0}.")
             put(MessageKeys.ERROR_GENERIC_LOGGED, "An error has occurred: {0}")
-            put(MessageKeys.UNKNOWN_COMMAND, "Command not found.")
-            put(MessageKeys.INVALID_SYNTAX, "§cInvalid syntax.")
+            put(MessageKeys.UNKNOWN_COMMAND, "Command not found. Type '/help' for help.")
+            put(MessageKeys.INVALID_SYNTAX, "§cInvalid syntax. Type '/help' for help.")
             put(MessageKeys.ERROR_PREFIX, "Error: ")
             put(MessageKeys.ERROR_PERFORMING_COMMAND, "An error has occurred while performing the command: {0}")
             put(MessageKeys.INFO_MESSAGE, "Info: {0}")
@@ -25,10 +25,16 @@ class VelocityCommandManagerSurrogate(proxyServer: ProxyServer, plugin: Any) :
             put(MessageKeys.MUST_BE_MAX_LENGTH, "Error: §c{num} must be at most {max} characters long.")
             put(MessageKeys.PLEASE_SPECIFY_AT_MOST, "Error: §c{num} must be at most {max}.")
             put(MessageKeys.PLEASE_SPECIFY_AT_LEAST, "Error: §c{num} must be at least {min}.")
-            put(MessageKeys.NOT_ALLOWED_ON_CONSOLE, "Error: This command is not allowed on the console.")
-            put(MessageKeys.COULD_NOT_FIND_PLAYER, "Error: Could not find player {0}.")
-            put(MessageKeys.NO_COMMAND_MATCHED_SEARCH, "No commands matched your search.")
-            put(MessageKeys.HELP_NO_RESULTS, "No results found.")
+            put(
+                MessageKeys.NOT_ALLOWED_ON_CONSOLE,
+                "Error: This command is not allowed on the console. Please use it in-game."
+            )
+            put(
+                MessageKeys.COULD_NOT_FIND_PLAYER,
+                "Error: Could not find player {0}. Please check the name and try again."
+            )
+            put(MessageKeys.NO_COMMAND_MATCHED_SEARCH, "No commands matched your search. Type '/help' for help.")
+            put(MessageKeys.HELP_NO_RESULTS, "No results found. Type '/help' for help.")
             put(MessageKeys.HELP_FORMAT, "§6{commandprefix}{command} {parameters} §8┃ §7{description}")
             put(
                 MessageKeys.HELP_HEADER,
