b852a93401e596d4bcab19a41c69a6009745e681
Mischmaschine <fixxertrixxer@gmail.com>
2022-07-26 23:23:34 +0200

    chore: add vararg to list

--- permissionsystem-bungeecord/src/main/kotlin/de/permission/extensions/_CommandSender.kt
+++ permissionsystem-bungeecord/src/main/kotlin/de/permission/extensions/_CommandSender.kt
@@ -1,11 +1,12 @@
 package de.permission.extensions
 
-import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.CommandSender
 import net.md_5.bungee.api.chat.TextComponent
 
-fun CommandSender.sendMessage(message: List<String>) {
-    message.forEach {
-        this.sendMessage(TextComponent("§8| ${ChatColor.of("#25e8a4")}ProxySystem §m| §7${it}"))
+fun CommandSender.sendMessage(vararg message: List<String>) {
+    message.forEach { list ->
+        list.forEach {
+            this.sendMessage(TextComponent("§8§l┃ §3PermissionSystem §8┃ §7${it}"))
+        }
     }
 }
